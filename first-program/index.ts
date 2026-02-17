b19a85e4cec2afc5459249026cff52c996075900
ratulislam46 <arfanratul46@gmail.com>
2026-02-18 01:29:31 +0600

    Added first program

---
+++ first-program/index.ts
@@ -0,0 +1,12 @@
+
+// tsc index.ts -> index.js ->node index.js 
+let userName: string = "Ratul islam";
+let age: number = 23
+console.log(userName);
+console.log(age);
+
+function addNumbers(num1: number, num2: number) {
+    return num1 + num2;
+}
+// addNumbers(20, 40)
+console.log(addNumbers(30, 40));
