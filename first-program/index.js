db16c8c25391ed20ec267e00d30d677f38f37f4e
ratulislam46 <arfanratul46@gmail.com>
2026-02-20 11:02:59 +0600

    Added Readme files content

---
+++ first-program/index.js
@@ -0,0 +1,10 @@
+// tsc index.ts -> index.js ->node index.js 
+var userName = "Ratul islam";
+var age = 23;
+console.log(userName);
+console.log(age);
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+// addNumbers(20, 40)
+console.log(addNumbers(30, 40));
