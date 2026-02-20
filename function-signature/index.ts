aba06ccdfa0d8628e6e9a4c79742d9328d0a4a44
ratulislam46 <arfanratul46@gmail.com>
2026-02-20 23:16:04 +0600

    Added funciton signature

--- function-signature/index.ts
+++ function-signature/index.ts
@@ -3,7 +3,6 @@ let userInfo1: () => void;
 let userInfo2: (name: string) => void;
 let userInfo3: (name: string, age: number) => void;
 
-
 userInfo1 = () => {
     console.log("This is Md Ratul Howlader");
 }
@@ -16,6 +15,6 @@ userInfo3 = (name, age) => {
     console.log(`Name: ${name}, Age: ${age}`);
 }
 
-userInfo1()
+// userInfo1()
 userInfo2("Md Ratul Howlader")
 userInfo3("Rabbi", 23)
